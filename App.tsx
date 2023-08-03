a91e7e6c5917389e3fbfebb9b9a9b9e5f255c348
Italovini223 <italovinicius2018@gmail.com>
2023-08-03 01:26:24 -0300

    ✨ feat: change project to Bare Workflow

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import OneSignal from 'react-native-onesignal';
+
 import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.setAppId('2a2b3e7b-507d-4ef5-832c-52465e2e9d62')
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
